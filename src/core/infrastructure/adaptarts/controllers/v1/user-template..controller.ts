4bae8c003a41016a92ddf0d74641fff86774e8e3
UnCompa <brandonddxd@gmail.com>
2024-10-16 15:54:56 -0500

    fix: :adhesive_bandage: Cambios para el controlador

--- src/core/infrastructure/adaptarts/controllers/v1/user-template..controller.ts
+++ src/core/infrastructure/adaptarts/controllers/v1/user-template..controller.ts
@@ -10,39 +10,45 @@ import {
   Put,
   Query,
 } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiCreatedResponse, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CreateUserDto } from 'src/core/application/dtos/create-user.dto';
 import { SendData } from 'src/core/application/dtos/sendData-user.dto';
 import { UpdateUserDto } from 'src/core/application/dtos/update-user.dto';
 import { UserService } from 'src/core/application/services/user.service';
 import { User } from 'src/core/domain/user.entity';
+import { apiStatus, createEntity } from 'src/utils/api/apiStatus';
 import { Validator } from 'src/utils/api/apiValidations';
 
-@ApiTags('/msa/users')
-@Controller('/msa/users')
+@ApiTags('pathDeTuServicio')
+@Controller('pathDeTuServicio')
 export class UserController {
   constructor(private userService: UserService) {}
-  @Post('1.0')
+  @ApiResponse(apiStatus.ok)
+  @ApiCreatedResponse(createEntity)
+  @Post('path')
   @HttpCode(201)
   async createUser(@Body() data: CreateUserDto): Promise<object> {
     return this.userService.create(data);
   }
 
-  @Get('1.0')
+  @ApiResponse(apiStatus.ok)
+  @Get('path')
   async getAllUsers(@Query() params): Promise<SendData | User[]> {
     const { limit, page } = params;
     return this.userService.findAll(limit, page);
   }
-
-  @Get('1.0/:id')
+  @ApiResponse(apiStatus.ok)
+  @ApiResponse(apiStatus.notFound)
+  @Get('path/:id')
   async getOneUser(@Param('id') id: string): Promise<User> {
     if (!Validator.isValidUUID(id)) {
       throw new BadRequestException('The "id" parameter must be a valid UUID.');
     }
     return this.userService.findOne(id);
   }
-
-  @Put('1.0/:id')
+  @ApiResponse(apiStatus.notFound)
+  @ApiResponse(apiStatus.badRequest)
+  @Put('path/:id')
   async updateUser(
     @Param('id') id: string,
     @Body() data: UpdateUserDto,
@@ -52,8 +58,8 @@ export class UserController {
     }
     return this.userService.update(id, data);
   }
-
-  @Delete('1.0/:id')
+  @ApiResponse(apiStatus.notFound)
+  @Delete('path/:id')
   async deleteUser(@Param('id') id: string): Promise<User> {
     if (!Validator.isValidUUID(id)) {
       throw new BadRequestException('The "id" parameter must be a valid UUID.');
